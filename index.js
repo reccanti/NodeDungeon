1733eda3c083aad8c73d9eb370f2bb46c5503849
Blair Wilcox <bwilcox@wayfair.com>
2019-12-28 00:02:02 -0500

    Create looping interface
    Creates a movement interface which continues to loop until the user
    presses "q". It also responds to "up", "down", "left", "right", and
    "input not recognized" events.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-console.log("Hello world");
+const readline = require("readline");
+
+// create interface
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// define inputs
+const INPUTS = {
+  UP: "w",
+  DOWN: "s",
+  LEFT: "a",
+  RIGHT: "d",
+  EXIT: "q"
+};
+
+// a function to handle movement input. It will loop and
+// continue to call itself until the user inputs "q" to exit.
+function getMovement() {
+  rl.question("Use WASD to move and press Q to exit: ", input => {
+    switch (input) {
+      case INPUTS.UP:
+        console.log("you moved up");
+        getMovement();
+        break;
+      case INPUTS.DOWN:
+        console.log("you moved down");
+        getMovement();
+        break;
+      case INPUTS.LEFT:
+        console.log("you moved left");
+        getMovement();
+        break;
+      case INPUTS.RIGHT:
+        console.log("you moved right");
+        getMovement();
+        break;
+      case INPUTS.EXIT:
+        exitProgram();
+        break;
+      default:
+        console.log("sorry, I didn't understand that");
+    }
+  });
+}
+
+// a function to close the program. By default, it just calls
+// the readline interface, but it might do more in the future.
+function exitProgram() {
+  console.log("leaving the program");
+  rl.close();
+}
+
+getMovement();
